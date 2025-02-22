e1cc5deebfee61f3db1b74c078721cac3740e11c
devikavarshney <devikavarshney0003@gmail.com>
2025-02-22 11:05:50 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About'
 import Technologies from './components/Technologies'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 function App() {
   return (
     <div className='overflox-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
